147d35820794f8b11eb0299a49d97c73cff283bd
Ahmed Mazen <ahmedmazenm@outlook.com>
2025-04-08 15:40:02 +0200

    removed unused shemas

--- backend_sanity/schemaTypes/index.js
+++ backend_sanity/schemaTypes/index.js
@@ -1,11 +1,9 @@
-import testimonials from "./testimonials";
 import work from "./work";
 import about from "./about";
 import workExperience from "./workExperience";
-import brands from "./brands";
 import contact from "./contact";
 import experiences from "./experiences";
 import skills from "./skills";
 
 
-export const schemaTypes = [testimonials , about , work , workExperience , brands , contact , experiences ,skills];
+export const schemaTypes = [ about , work , workExperience  , contact , experiences ,skills];
